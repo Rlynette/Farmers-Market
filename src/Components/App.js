760b3e5cd82cd2e403d82d0d4f187c2c866faffc
Rlynette <lynette.ronnoh@student.moringaschool.com>
2022-10-05 21:17:08 +0300

    rewrite components

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,19 +1,1 @@
-import React from "react";
-// import Product from "./Product";
-// import ProductForm from "./ProductForm";
-import ListInCart from "./ListInCart"
 
-
-function App() {
-  return (
-    <div className="App">
-         <h3>Home Page</h3>
-      <ListInCart />
-      {/* <Product /> */}
-      {/* <ProductForm /> */}
-      </div>
-  );
-}
-
-
-export default App;
