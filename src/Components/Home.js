b1f09a553dea484300813cdd378262e45402b1ad
Rlynette <lynette.ronnoh@student.moringaschool.com>
2022-10-11 13:47:05 +0300

    completed project

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,20 +6,21 @@ import img3 from "./images1/img3.jpg";
 
 function Home() {
 
-return (
-    <div className="images">
-       <h2> Home Page </h2> 
-       <div className="image">
-    <div className= "image1">
-        <img src={img1} alt=""/></div>
-    <div className= "image2">
-        <img src={img2} alt=""/></div>
-    <div className= "image3">
-        <img src={img3} alt=""/>
-    </div>
-    </div>
-    </div>
-    
-)
+    return (
+        <div className="images">
+            <h2> Home Page </h2>
+            <p>We go beyond borders to ensure you get fresh produce from the farm. Our customers satisfaction is our mission.</p>
+            <div className="image">
+                <div className="image1">
+                    <img src={img1} alt="" /></div>
+                <div className="image2">
+                    <img src={img2} alt="" /></div>
+                <div className="image3">
+                    <img src={img3} alt="" />
+                </div>
+            </div>
+        </div>
+
+    )
 }
 export default Home;
